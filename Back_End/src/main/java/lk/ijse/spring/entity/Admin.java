3a8fb83cf80bfa052e8527637bf9e92eba15e36a
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-16 22:57:45 +0530

    create Admin entity

---
+++ Back_End/src/main/java/lk/ijse/spring/entity/Admin.java
@@ -0,0 +1,21 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class Admin {
+    @Id
+    private String adminId;
+    private String email;
+    private String userName;
+    private String password;
+}
