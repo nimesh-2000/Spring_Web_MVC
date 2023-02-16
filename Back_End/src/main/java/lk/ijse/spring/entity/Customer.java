4c46a7f2110a504e120de2d224fb953fb52a3f17
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-16 22:53:35 +0530

    create customer entity

---
+++ Back_End/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -0,0 +1,29 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class Customer {
+    @Id
+    private String customerId;
+    private String cusName;
+    private String nic;
+    private String contactNo;
+    private String address;
+    private String email;
+    private String password;
+    private String drivingLicenceNumber;
+    private String imageLocation;
+
+
+}
