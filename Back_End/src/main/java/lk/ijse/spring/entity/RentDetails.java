64a5050326d6f0a6611375a73d36947f938ba253
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-16 23:24:03 +0530

    create RentDetails enetity

---
+++ Back_End/src/main/java/lk/ijse/spring/entity/RentDetails.java
@@ -0,0 +1,25 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class RentDetails {
+    @EmbeddedId
+    private RentalDetails_PK id;
+    @ManyToOne
+    private Driver driver_id;
+    private String rental_status;
+    private String payment_slip;
+}
