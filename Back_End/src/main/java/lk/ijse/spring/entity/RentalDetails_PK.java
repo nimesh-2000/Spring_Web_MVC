f6a15cf288d6188149961236eab5dbfdb72a9847
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-17 00:09:37 +0530

    content u

--- Back_End/src/main/java/lk/ijse/spring/entity/RentalDetails_PK.java
+++ Back_End/src/main/java/lk/ijse/spring/entity/RentalDetails_PK.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Embeddable
 public class RentalDetails_PK  implements Serializable {
     private String rent_id;
     private String car_id;
