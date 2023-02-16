ab7009d864b23bf4b9947fd497f8d2a62a21f471
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-16 23:34:33 +0530

    create payment entity

---
+++ Back_End/src/main/java/lk/ijse/spring/entity/Payment.java
@@ -0,0 +1,32 @@
+package lk.ijse.spring.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
+public class Payment {
+    @Id
+    private String paymentId;
+    private LocalDate date;
+    private double rent_amount;
+    private long extra_mileage;
+    private double cost_per_extra_km;
+    private double damage_cost;
+    private String damageDescription;
+    private String payment_status;
+
+    @OneToOne
+    private Rental rentalId;
+}
