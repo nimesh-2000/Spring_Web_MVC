120fda0b3b635b9f74dd46e2252000c43e4c1853
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-18 11:51:10 +0530

    add anotaion

--- Back_End/src/main/java/lk/ijse/spring/advice/AppWideExceptionHandler.java
+++ Back_End/src/main/java/lk/ijse/spring/advice/AppWideExceptionHandler.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+@RestControllerAdvice
+@CrossOrigin
 public class AppWideExceptionHandler {
-
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({RuntimeException.class})
     public ResponseUtil handleException(RuntimeException e){
         return new ResponseUtil("500",e.getMessage(),null);
     }
