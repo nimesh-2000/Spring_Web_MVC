289db34d34bd886fbf1a27b702994a9fd6eea6f6
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-18 11:30:43 +0530

    add allarg and noarg constructors

--- Back_End/src/main/java/lk/ijse/spring/utill/ResponseUtil.java
+++ Back_End/src/main/java/lk/ijse/spring/utill/ResponseUtil.java
@@ -4,4 +4,13 @@ public class ResponseUtil {
     private String code;
     private String message;
     private Object data;
+
+    public ResponseUtil() {
+    }
+
+    public ResponseUtil(String code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
 }
