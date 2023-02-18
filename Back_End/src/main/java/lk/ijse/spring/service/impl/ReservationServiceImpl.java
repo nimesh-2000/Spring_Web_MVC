c2953fe712c9f1f7e0c4569cf2200a051e8e64cf
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-18 22:11:50 +0530

    content -u

--- Back_End/src/main/java/lk/ijse/spring/service/impl/ReservationServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/ReservationServiceImpl.java
@@ -1,4 +1,48 @@
 package lk.ijse.spring.service.impl;
 
-public class ReservationServiceImpl {
+import lk.ijse.spring.dto.ReservationDTO;
+import lk.ijse.spring.service.ReservationService;
+
+import java.util.List;
+
+public class ReservationServiceImpl implements ReservationService {
+    @Override
+    public String generateReservationId() {
+        return null;
+    }
+
+    @Override
+    public void requestReservation(ReservationDTO ReservationDTO) {
+
+    }
+
+    @Override
+    public void updateReservationStatus(String reserve_id, String driver_id, String status) {
+
+    }
+
+    @Override
+    public List<ReservationDTO> getAllPendingReservation() {
+        return null;
+    }
+
+    @Override
+    public ReservationDTO getReservationDetail(String id) {
+        return null;
+    }
+
+    @Override
+    public List<ReservationDTO> getAllTodayReservation() {
+        return null;
+    }
+
+    @Override
+    public List<ReservationDTO> getAllTodayPickUps() {
+        return null;
+    }
+
+    @Override
+    public List<ReservationDTO> getCustomerReservationByStatus(String id, String status) {
+        return null;
+    }
 }
