c2081b82286d6d04777033934de809b8fd011db0
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-18 22:07:51 +0530

    content -u

--- Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,38 @@
 package lk.ijse.spring.service.impl;
 
-public class CustomerServiceImpl {
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public String updateCustomer(CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public CustomerDTO getCustomerDetail(String id) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomerDetail() {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getTodayRegisteredCustomers() {
+        return null;
+    }
 }
