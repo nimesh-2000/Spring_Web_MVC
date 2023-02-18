e2fc079150b5289f56e601804ee6689f92d915dc
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-18 22:04:15 +0530

    contenr -u

--- Back_End/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
@@ -1,4 +1,11 @@
 package lk.ijse.spring.service.impl;
 
-public class AdminServiceImpl {
+import lk.ijse.spring.dto.AdminDTO;
+import lk.ijse.spring.service.AdminService;
+
+public class AdminServiceImpl implements AdminService {
+    @Override
+    public AdminDTO checkAdminLogIn(String id, String password) {
+        return null;
+    }
 }
--- Back_End/src/main/java/lk/ijse/spring/service/impl/ReservationServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/ReservationServiceImpl.java
@@ -1,4 +1,4 @@
 package lk.ijse.spring.service.impl;
 
-public class ResrvationServiceImpl {
+public class ReservationServiceImpl {
 }
