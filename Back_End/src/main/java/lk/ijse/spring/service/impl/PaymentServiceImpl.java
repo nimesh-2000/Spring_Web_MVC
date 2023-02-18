6ae8c6e222a693c941c16f3be42c30a25a7ee16c
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-18 22:10:26 +0530

    content -u

--- Back_End/src/main/java/lk/ijse/spring/service/impl/PaymentServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/PaymentServiceImpl.java
@@ -1,4 +1,28 @@
 package lk.ijse.spring.service.impl;
 
-public class PaymentServiceImpl {
+import lk.ijse.spring.dto.PaymentDTO;
+import lk.ijse.spring.service.PaymentService;
+
+import java.util.List;
+
+public class PaymentServiceImpl implements PaymentService {
+    @Override
+    public String generateReservationBillIdId() {
+        return null;
+    }
+
+    @Override
+    public void makePaymentForReservation(PaymentDTO paymentDTO) {
+
+    }
+
+    @Override
+    public String getIncomeByDate(String type, String start_date, String end_date) {
+        return null;
+    }
+
+    @Override
+    public List<PaymentDTO> getTodayIncomeList() {
+        return null;
+    }
 }
