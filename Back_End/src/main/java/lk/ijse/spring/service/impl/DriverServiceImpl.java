789e0604251a46b44708a5ebfce2188bed0458e0
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-18 22:09:13 +0530

    content -u

--- Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
+++ Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
@@ -1,4 +1,43 @@
 package lk.ijse.spring.service.impl;
 
-public class DriverServiceImpl {
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.service.DriverService;
+
+import java.util.List;
+
+public class DriverServiceImpl implements DriverService {
+    @Override
+    public DriverDTO checkDriverLogIn(String name, String password) {
+        return null;
+    }
+
+    @Override
+    public void saveDriver(DriverDTO driverDTO) {
+
+    }
+
+    @Override
+    public void UpdateDriver(DriverDTO driverDTO) {
+
+    }
+
+    @Override
+    public void deleteDriver(String id) {
+
+    }
+
+    @Override
+    public DriverDTO getDriverDetail(String id) {
+        return null;
+    }
+
+    @Override
+    public List<DriverDTO> getAllDriverDetail() {
+        return null;
+    }
+
+    @Override
+    public List<DriverDTO> getTodayAvailableAndOccupiedDrivers(String status) {
+        return null;
+    }
 }
