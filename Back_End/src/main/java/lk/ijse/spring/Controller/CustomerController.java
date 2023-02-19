b9e333a3c3b44c3a927d5e0789b86348db3d9704
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-19 23:16:15 +0530

    content -u

--- Back_End/src/main/java/lk/ijse/spring/Controller/CustomerController.java
+++ Back_End/src/main/java/lk/ijse/spring/Controller/CustomerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/customer")
 @RestController
-public class Customer {
+public class CustomerController {
 
     @GetMapping
     public String get(){
