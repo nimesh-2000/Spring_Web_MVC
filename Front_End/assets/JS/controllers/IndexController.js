97fc4b34a7ec3723f641d4a984e49383868bc9cb
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-22 11:39:55 +0530

    content -u

--- Front_End/assets/JS/controllers/IndexController.js
+++ Front_End/assets/JS/controllers/IndexController.js
@@ -1,6 +1,50 @@
 $("#h").click(function(){
-    $("#hero").css('display','block');
+    $("#home").css('display','block');
     $("#Clogin").css('display','none');
+    $("#Csign").css('display','none');
+    $("#vfeet").css('display','none');
+    $("#chO").css('display','none');
+    $("#ac").css('display','none');
+    $("#dash").css('display','none');
+    $("#dashSEM").css('display','none');
+});
+
+$("#vF").click(function(){
+    $("#home").css('display','none');
+    $("#Clogin").css('display','none');
+    $("#Csign").css('display','none');
+    $("#vfeet").css('display','block');
+    $("#chO").css('display','none');
+    $("#ac").css('display','none');
+    $("#dash").css('display','none');
+    $("#dashSEM").css('display','none');
+});
+
+$("#A").click(function(){
+    $("#home").css('display','none');
+    $("#Clogin").css('display','none');
+    $("#Csign").css('display','none');
+    $("#vfeet").css('display','none');
+    $("#chO").css('display','none');
+    $("#ac").css('display','block');
+    $("#dash").css('display','none');
+    $("#dashSEM").css('display','none');
+});
+$("#getS").click(function (){
+    $("#home").css('display','none');
+    $("#Clogin").css('display','block');
+    $("#Csign").css('display','none');
+    $("#vfeet").css('display','none');
+    $("#chO").css('display','none');
+    $("#ac").css('display','none');
+    $("#dash").css('display','none');
+    $("#dashSEM").css('display','none');
+});
+
+
+$("#get").click(function (){
+    $("#home").css('display','none');
+    $("#Clogin").css('display','block');
     $("#Csign").css('display','none');
     $("#vfeet").css('display','none');
     $("#chO").css('display','none');
