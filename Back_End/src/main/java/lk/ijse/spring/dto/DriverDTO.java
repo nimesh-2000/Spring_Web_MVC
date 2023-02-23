de484cf504af179a3f0e3bbeb2331ecb35be3471
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-23 11:11:50 +0530

    create driver dto

--- Back_End/src/main/java/lk/ijse/spring/dto/DriverDTO.java
+++ Back_End/src/main/java/lk/ijse/spring/dto/DriverDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class DriverDTO {
+    private String driverId;
+    private String name;
+    private String nic;
+    private String password;
 }
