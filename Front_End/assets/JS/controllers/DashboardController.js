3853dd1f236786a1759b7e599447b0e3b84346eb
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-23 23:40:57 +0530

    content -u

---
+++ Front_End/assets/JS/controllers/DashboardController.js
@@ -0,0 +1,86 @@
+$("#d").click(function(){
+    $("#dashSE").css('display','block');
+    $("#carDash").css('display','none');
+    $("#customerVerification").css('display','none');
+    $("#rentVerification").css('display','none');
+    $("#payment").css('display','none');
+    $("#employeeS").css('display','none');
+    $("#viewCar").css('display','none');
+    $("#home").css('display','none');
+    $("#header").css('display','none');
+});
+$("#c").click(function(){
+    $("#dashSE").css('display','none');
+    $("#carDash").css('display','none');
+    $("#customerVerification").css('display','block');
+    $("#rentVerification").css('display','none');
+    $("#payment").css('display','none');
+    $("#employeeS").css('display','none');
+    $("#viewCar").css('display','none');
+    $("#home").css('display','none');
+    $("#header").css('display','none');
+});
+$("#ca").click(function(){
+    $("#dashSE").css('display','none');
+    $("#carDash").css('display','block');
+    $("#customerVerification").css('display','none');
+    $("#rentVerification").css('display','none');
+    $("#payment").css('display','none');
+    $("#employeeS").css('display','none');
+    $("#viewCar").css('display','none');
+    $("#home").css('display','none');
+    $("#header").css('display','none');
+});
+$("#r").click(function(){
+    $("#dashSE").css('display','none');
+    $("#carDash").css('display','none');
+    $("#customerVerification").css('display','none');
+    $("#rentVerification").css('display','block');
+    $("#payment").css('display','none');
+    $("#employeeS").css('display','none');
+    $("#viewCar").css('display','none');
+    $("#home").css('display','none');
+    $("#header").css('display','none');
+});
+$("#p").click(function(){
+    $("#dashSE").css('display','none');
+    $("#carDash").css('display','none');
+    $("#customerVerification").css('display','none');
+    $("#rentVerification").css('display','none');
+    $("#payment").css('display','block');
+    $("#employeeS").css('display','none');
+    $("#viewCar").css('display','none');
+    $("#home").css('display','none');
+    $("#header").css('display','none');
+});
+$("#e").click(function(){
+    $("#dashSE").css('display','none');
+    $("#carDash").css('display','none');
+    $("#customerVerification").css('display','none');
+    $("#rentVerification").css('display','none');
+    $("#payment").css('display','none');
+    $("#employeeS").css('display','block');
+    $("#viewCar").css('display','none');
+    $("#home").css('display','none');
+    $("#header").css('display','none');
+});
+$("#logOut").click(function(){
+    $("#dashSE").css('display','none');
+    $("#carDash").css('display','none');
+    $("#customerVerification").css('display','none');
+    $("#rentVerification").css('display','none');
+    $("#payment").css('display','none');
+    $("#employeeS").css('display','none');
+    $("#viewCar").css('display','none');
+    $("#home").css('display','block');
+    $("#header").css('display','block');
+
+    // $("#carDash").css('display','none');
+    // $("#customerVerification").css('display','none');
+    // $("#rentVerification").css('display','none');
+    // $("#payment").css('display','none');
+    // $("#employeeS").css('display','block');
+    // $("#viewCar").css('display','none');
+    // $("#home").css('display','none');
+    // $("#header").css('display','none');
+});
