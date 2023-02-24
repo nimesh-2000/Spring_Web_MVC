deea776b4e2c7a3638a07606f4e5d204d3a1fe61
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-24 14:29:51 +0530

    create login controller

---
+++ Back_End/src/main/java/lk/ijse/spring/Controller/LoginController.java
@@ -0,0 +1,27 @@
+package lk.ijse.spring.Controller;
+
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.service.CustomerService;
+import lk.ijse.spring.utill.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RequestMapping("/login")
+@RestController
+public class LoginController {
+
+    @Autowired
+    CustomerService service;
+
+    @GetMapping(params = "email")
+    public ResponseUtil checkCustomer(String email) {
+        System.out.println(email);
+        CustomerDTO customerDTO = service.searchCustomerByEmail(email);
+        System.out.println(customerDTO);
+        return new ResponseUtil("200", "Login Success", customerDTO);
+    }
+}
