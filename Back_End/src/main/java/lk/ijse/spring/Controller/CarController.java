3d8352aa1d381492afe7d2bcf9d4cce338740de7
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-27 20:53:25 +0530

    content -u

--- Back_End/src/main/java/lk/ijse/spring/Controller/CarController.java
+++ Back_End/src/main/java/lk/ijse/spring/Controller/CarController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 
 @CrossOrigin
@@ -19,15 +20,34 @@ import java.util.ArrayList;
 @RestController
 public class CarController {
 
-    @Autowired
-    CarService service;
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseUtil saveCar(@RequestBody CarDTO carDTO){
-        service.saveCar(carDTO);
-        return new ResponseUtil("200",carDTO.toString()+ " Added",null);
+   @Autowired
+   CarService service;
+@PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+public ResponseUtil addVehicle(@RequestPart("vImageFile") MultipartFile[] file, @RequestPart("vehicle") CarDTO carDTO) {
+
+
+    for (MultipartFile myFile : file) {
+
+        try {
+            String projectPath = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile().getParentFile().getAbsolutePath();
+            File uploadsDir = new File(projectPath + "/uploads");
+            uploadsDir.mkdir();
+            myFile.transferTo(new File(uploadsDir.getAbsolutePath() + "/" + myFile.getOriginalFilename()));
+            System.out.println(projectPath);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+            return new ResponseUtil("500", "Registration Failed.Try Again Latter", null);
+        }
     }
 
-    @PutMapping(path = "/uploadImg/{registrationId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+
+
+
+    service.saveCar(carDTO);
+    return new ResponseUtil("200", "Registration Successfully....", carDTO);
+}
+
+    @PostMapping(path = "/uploadImg/{registrationId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseUtil uploadImagesAndPath(@RequestPart("image_1") MultipartFile image_1, @RequestPart("image_2") MultipartFile image_2, @RequestPart("image_3") MultipartFile image_3, @RequestPart("image_4") MultipartFile image_4, @PathVariable String registrationId) {
         try {
 
@@ -40,10 +60,10 @@ public class CarController {
             image_3.transferTo(new File(uploadsDir.getAbsolutePath() + "\\" + image_3.getOriginalFilename()));
             image_4.transferTo(new File(uploadsDir.getAbsolutePath() + "\\" + image_4.getOriginalFilename()));
 
-            String carFrontViewPath = projectPath + "\\CarImage" + image_1.getOriginalFilename();
-            String carBackViewPath = projectPath + "\\CarImage" + image_2.getOriginalFilename();
-            String carSideViewPath = projectPath + "\\CarImage" + image_3.getOriginalFilename();
-            String carInteriorViewPath = projectPath + "\\CarImage" + image_4.getOriginalFilename();
+            String carFrontViewPath =  image_1.getOriginalFilename();
+            String carBackViewPath =  image_2.getOriginalFilename();
+            String carSideViewPath =  image_3.getOriginalFilename();
+            String carInteriorViewPath = image_4.getOriginalFilename();
 
             service.uploadCarImages(carFrontViewPath, carBackViewPath, carSideViewPath, carInteriorViewPath, registrationId);
 
