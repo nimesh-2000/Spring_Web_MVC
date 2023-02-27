ee8c14e8d2fc9dcc74166928ffee4c8b27ca8fba
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-02-27 14:58:53 +0530

    content -u

--- Back_End/src/main/java/lk/ijse/spring/Controller/AdminController.java
+++ Back_End/src/main/java/lk/ijse/spring/Controller/AdminController.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.Controller;
 
 import lk.ijse.spring.dto.AdminDTO;
 import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.service.AdminService;
 import lk.ijse.spring.service.CarService;
 import lk.ijse.spring.utill.ResponseUtil;
@@ -18,5 +19,12 @@ public class AdminController {
     public ResponseUtil saveAdmin(@RequestBody AdminDTO dto){
         service.saveAdmin(dto);
         return new ResponseUtil("200",dto.toString()+ " Added",null);
+    }
+    @GetMapping(params = "email")
+    public ResponseUtil checkAdmin(String email) {
+        System.out.println(email);
+        AdminDTO adminDTO = service.searchAdminByEmail(email);
+        System.out.println(adminDTO);
+        return new ResponseUtil("200", "Login Success", adminDTO);
     }
 }
