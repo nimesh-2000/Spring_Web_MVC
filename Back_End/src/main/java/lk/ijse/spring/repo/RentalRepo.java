e440a9d1ba5c55044dda84efd3aeccb4137b8c99
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-03-01 11:23:34 +0530

    content -u

--- Back_End/src/main/java/lk/ijse/spring/repo/RentalRepo.java
+++ Back_End/src/main/java/lk/ijse/spring/repo/RentalRepo.java
@@ -2,9 +2,18 @@ package lk.ijse.spring.repo;
 
 import lk.ijse.spring.entity.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 
 public interface RentalRepo extends JpaRepository<Rental,String> {
     @Query(value = "SELECT rentalId FROM Rental ORDER BY rentalId DESC LIMIT 1", nativeQuery = true)
     String generateRentalId();
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Rental SET payment_slip=:payment_slip WHERE rentalId=:rentalId", nativeQuery = true)
+    void updatePaymentSlipFilePaths(@Param("payment_slip") String payment_slip, @Param("rentalId") String rentalId);
 }
