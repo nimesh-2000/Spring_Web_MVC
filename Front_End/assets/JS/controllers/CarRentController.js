a36a3c1db27f6a7d39d4236f6fa633aa8de5b82e
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-03-04 22:56:24 +0530

    add rental

--- Front_End/assets/JS/controllers/CarRentController.js
+++ Front_End/assets/JS/controllers/CarRentController.js
@@ -1,24 +1,21 @@
+
 var p;
 var r;
 
 $("#btnsendReq").click(function () {
+
     addRental();
 });
 
-function addRental(driver) {
+function addRental() {
 
     let driverOption = $("#selectDriver").val();
-
 
     p = $("#txtFromDate").val();
     r = $("#txtToDate").val();
 
 
-    var Rdata = new FormData();
-
     let paymentSlipName = $("#lossDP2")[0].files[0].name;
-    let paymentSlipFile = $("#lossDP2")[0].files[0];
-
 
     let registrationId = $("#CheckReTable").children().eq(0).children(":eq(0)").text();
     let onHold = $("#CheckReTable").children().eq(0).children(":eq(4)").text();
@@ -30,9 +27,8 @@ function addRental(driver) {
     let returnLocation = $("#txtReturnL").val();
     let slipImgPath = paymentSlipName;
     let statusOfReq = "Pending";
-    let cusId =   $("#anic").text();
-    // let getDriverId = getDriverId(rentId);
-// console.log(driver);
+    let cusId = $("#anic").text();
+    let driverId = $("#txtDriverId").val();
 
     let rent = {
         rentalId: rentId,
@@ -45,20 +41,18 @@ function addRental(driver) {
         rental_status: statusOfReq,
         total_damage_waiver_payment: onHold,
         cusNic: cusId,
-        driver_id: driver,
-        registrationId: registrationId
+        driverId: driverId,
+        registrationID: registrationId
     }
-
-    Rdata.append("rImageFile", paymentSlipFile);
-    Rdata.append("carRental", new Blob([JSON.stringify(rent)], {type: "application/json"}))
+    console.log(JSON.stringify(rent))
 
     $.ajax({
         url: baseURL + "rental",
         method: "POST",
         async: true,
-        contentType: false,
-        processData: false,
-        data: Rdata,
+        contentType: "application/json",
+
+        data: JSON.stringify(rent),
         success: function (resp) {
             sendRentImagePath(rentId);
             Swal.fire({
@@ -68,7 +62,7 @@ function addRental(driver) {
                 showConfirmButton: false,
                 timer: 1500
             });
-
+            // updateCarAvai();
         },
         error: function (error) {
             Swal.fire({
@@ -79,26 +73,19 @@ function addRental(driver) {
                 timer: 1500
             });
         }
-    })
+    });
 }
-
-
-// =====================================================================
-
 function sendRentImagePath(rentId) {
     var data = new FormData();
-
-    // let file = $("#lossDP2")[0].files[0];
-    // let fileName = rentId + "-payment_slip-" + $("#lossDP2")[0].files[0].name;
-    // data.append("payment_slip", file, fileName);
 
     let file = $("#lossDP2")[0].files[0];
     let fileName = rentId + "-payment_slip-" +  $("#lossDP2")[0].files[0].name;
 
     data.append("payment_slip", file, fileName);
 
+
     $.ajax({
-        url: baseURL + "rental/uploadImg" + rentId,
+        url: baseURL + "rental/uploadImg/" + rentId,
         method: 'post',
         async: true,
         contentType: false,
@@ -126,99 +113,26 @@ function sendRentImagePath(rentId) {
     });
 }
 
-//get added driver ids and updated driver availability
-// function a(id) {
-//     alert(id);
-//     $.ajax({
-//         url: baseURL + "driver",
-//         dataType: "Json",
-//         method: "get",
-//         async:false,
-//         success: function (resp) {
-//             let driver;
-//             for (const d of resp.data) {
-//                 if (id == d.driver_id) {
-//                     driver = {
-//                         driver_id:d.driver_id,
-//                         nic: d.nic,
-//                         name: d.name,
-//                         drivingLicenceNum:d.drivingLicenceNum,
-//                         availability: "Unavailable"
-//                     }
-//                 }
-//             }
-//
-//
-//             //update driver
-//             $.ajax({
-//                 url: baseURL + "driver",
-//                 method: "put",
-//                 data: JSON.stringify(driver),
-//                 contentType: "application/json",
-//                 async:false,
-//                 success: function (resp) {
-//                     alert(resp.message);
-//                 },
-//                 error: function (error) {
-//                     let prase = JSON.parse(error.responseText);
-//                     alert(prase.message);
-//
-//                 }
-//
-//             });
-//         },
-//         error: function (error) {
-//             let prase = JSON.parse(error.responseText);
-//             alert(prase.message);
-//
-//         }
-//     });
-// }
 
-
-// =========================================================
-
-
-// function getAllAvailableDriver(){
-// let available="Available";
-// $.ajax({
-//     url: baseURL+"driver/availability/drivers",
-//     dataType:"json",
-//     method: "get",
-//     success: function (resp) {
-//         console.log(resp)
-//        alert(resp);
-//             driverId = resp.data.driver_id;
-//             driverIds.push(driverId);
-//             //set all available drivers to array
-//
-//     }
-//
-// });
-
-// ========================================
 
 $('#selectDriver').change(function () {
     let driverOption = $('#selectDriver').find('option:selected').text();
-    console.log(driverOption);
-    if (driverOption==="Driver"){
-        randomDriver();
+    // console.log(driverOption);
+    if (driverOption == "Driver"){
+        // randomDriver();
     }
     else {
         addRental("None");
     }
 })
-
+randomDriver();
 function randomDriver(){
     $.ajax({
         url: baseURL + "driver/randomDriver",
-        method: "get",
+        dataType:"json",
         success: function (resp) {
-            for (let driver of resp.data){
-                console.log(driver);
-                addRental(driver);
-            }
-
+            // console.log(resp.data.driver_id)
+            $("#driverID").val(resp.data.driverId);
         }
 
     });
