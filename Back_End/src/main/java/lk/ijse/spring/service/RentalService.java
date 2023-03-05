5c4683708203b3ac2cc965a91b5c9e58c43dc82b
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-03-05 17:21:42 +0530

    content -u

--- Back_End/src/main/java/lk/ijse/spring/service/RentalService.java
+++ Back_End/src/main/java/lk/ijse/spring/service/RentalService.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.dto.RentalDTO;
 
 import java.util.ArrayList;
@@ -15,5 +16,7 @@ public interface RentalService {
      ArrayList<RentalDTO> getAllRentals();
 
      void deleteRental(String id);
+
+     void updateRental(RentalDTO rentalDTO);
 
 }
