7d2eae871afe70d0aefee11c2c28c7151f6041f8
nimesh-2000 <nimeshdenuwan2000@gmail.com>
2023-03-08 00:18:46 +0530

    add controller

---
+++ Back_End/src/main/java/lk/ijse/spring/Controller/PaymentController.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RequestMapping("/payment")
+@RestController
+public class PaymentController {
+}
